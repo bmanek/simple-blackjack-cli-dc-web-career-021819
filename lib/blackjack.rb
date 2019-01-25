a06c9bc23eeab19536aa92db1c94acc84afb5c98
Ben Manekin <ben.manekin@gmail.com>
2019-01-25 17:47:25 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,61 @@
+require 'pry'
+
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
-  # code #deal_card here
+
+def deal_card(card = 0)
+  new_card = rand(1..11)
+  card += new_card
+  puts card
 end
 
-def display_card_total
-  # code #display_card_total here
+
+def display_card_total(card)
+  puts "Your cards add up to #{card}"
 end
+
 
 def prompt_user
-  # code #prompt_user here
+  puts "Type 'h' to hit or 's' to stay"
 end
+
 
 def get_user_input
-  # code #get_user_input here
+  decision = gets
 end
 
-def end_game
-  # code #end_game here
+
+def end_game(card_total)
+  puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
+
 
 def initial_round
-  # code #initial_round here
+  first_hand = deal_card(deal_card)
+  display_card_total(first_hand)
 end
 
-def hit?
-  # code hit? here
+
+def hit?(current_card_total)
+  prompt_user
+  get_user_input
+    if decision == "s"
+      display_card_total
+    elsif decision == "h"
+      deal_card
+  #   display_card_total(?)
+    else
+      invalid_command
+    end
+  display_card_total
 end
+      
 
 def invalid_command
-  # code invalid_command here
+  puts "Please enter a valid command"
+  prompt_user
 end
 
 #####################################################
@@ -42,3 +66,4 @@ def runner
   # code runner here
 end
     
+deal_card
